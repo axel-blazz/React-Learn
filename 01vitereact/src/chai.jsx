42f46722e039e6e7d20a4f0ecf550102151a9d1c
Anish Sinha <anish28april@gmail.com>
2024-06-28 16:58:36 +0530

    git flow and structure

--- 01vitereact/src/chai.jsx
+++ 01vitereact/src/chai.jsx
@@ -1,10 +1,17 @@
 function Chai() {
+    const username = "Chaiwala"
+
     return (
         <div>
-            <h1>Chai</h1>
+            <h1>Chai {username}</h1>
             <p>Chai is a popular library for writing tests in JavaScript.</p>
         </div>
     );
 }
+
+// {username} is an evaluated expression, it means ki koi alag se evaluation nhi hoga, jo bhi value hai vo direct render hoga
+// eg {if (true) {console.log('hello')}} // ye nhi chalega
+// eg {console.log('hello')} // ye chalega
+
 
 export default Chai;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+const CustomApp = () => {
+  return (
+    <h1>App bina import kiye</h1>
+  )
+}
+
+const reactElement = {
+  type: 'a',
+  props: {
+      href: 'https://www.google.com',
+      target: '_blank',
+      
+  },
+  children: 'Click me to go to Google'
+}
+
+// app parse hoga aur reactElement parse hoga
+const anotherUser = 'Chaiwala'
+const ReactElement = React.createElement(
+  'a',
+  {
+    href: 'https://www.google.com',
+    target: '_blank',
+  },
+  'Click me to go to Google',
+  anotherUser
+
+)
+
 ReactDOM.createRoot(document.getElementById('root')).
 render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>
+  ReactElement
+  
 )
